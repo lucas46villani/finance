1b8bf03a4a762c75e56c80089f7fce934244518c
lucas46villani <lucasvillani@yahoo.com.ar>
2026-03-04 10:12:55 -0300

    Add files via upload
    First commit

---
+++ Descarga_Precios.py
@@ -0,0 +1,21 @@
+#%% Stock's Download
+import pandas as pd 
+import yfinance as yf            
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+
+#%%Bajo COTIZACION DE ACCIONES de Yahoo Finance
+
+def data(tickets, start=None):
+    '''Dowload price and volume stocks from yahoofinance.
+    Parameters:
+    tickets: Ticket names as string
+    start: The first day we want to have information.  
+    '''  
+    mktl=yf.download(tickets, start=start)
+    return mktl      
+
+
+
